1110589621cd4785ad54b40c10a82ee0d97ec97f
edbert khovey <64061171+evoreign@users.noreply.github.com>
2020-10-20 18:55:09 +0700

    Add files via upload

--- scraperspushgit.js
+++ scraperspushgit.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const xlsx = require("xlsx");
-
 async function scrapeJadwal(url){
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
@@ -12,18 +12,16 @@ async function scrapeJadwal(url){
         page.waitForNavigation(),
         page.click('#btnSubmit')
     ])
-    await page.waitFor(2000);
-    const data = await page.$$eval('.box-container tr', trs => trs.map((tr) => {
+    page.waitForTimeout(1000)
+    const data = await page.$$eval(' .viconTable tbody tr', trs => trs.map((tr) => {
         return tr.innerText;
       }));
-      console.log(data);
-      const aoalinks = data.map(l => [l]);
+    console.log(data);
+    const aoalinks = data.map(l => [l]);
     const wb = xlsx.utils.book_new();
     const ws = xlsx.utils.aoa_to_sheet(aoalinks);
     xlsx.utils.book_append_sheet(wb,ws);
     xlsx.writeFile(wb,"output.txt");
-    console.log({srcTxt});
 
-    
 }
 scrapeJadwal('https://myclass.apps.binus.ac.id/Home/Index');
